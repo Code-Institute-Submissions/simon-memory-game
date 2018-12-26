d1a41f9a7cbfcb399c3ca50fb88a2fc816191591
Ezat <ezat_test@hotmail.com>
2018-12-26 16:11:49 +0000

    Wrote some more tests. Implemented simon functionality so it now creates and plays a simon sequence and accepts & checks user input. Also, some minor styling changes.

--- Tests/spec/simonSpec.js
+++ Tests/spec/simonSpec.js
@@ -1,11 +1,74 @@
-describe("Simon", function(){
-    describe("Game Logic Test", function(){
-        it("Should return 'FizzBuzz'", function(){
-            expect(fizzBuzz(15)).toBe("FizzBuzz");
+describe("Game Logic Tests", function(){
+    
+    // Make sure a valid color is returned
+    describe("Defensive programming tests", function(){
+        // Make sure that colour returned is 'green', 'red', 'yellow' or 'blue'
+        var pattern = /(green|red|yellow|blue)/;
+
+        it("should return 'green', 'red', 'yellow' or 'blue'", function(){
+            expect(generatePattern()).toMatch(pattern);
         });
-        //Defensive programming
-        it("Should return a Error if we don't provide a number", function(){
-            expect(fizzBuzz("Cheese")).toBe("Error!");
+        it("should return 'green', 'red', 'yellow' or 'blue'", function(){
+            expect(generatePattern()).toMatch(pattern);
+        });
+        it("should return 'green', 'red', 'yellow' or 'blue'", function(){
+            expect(generatePattern()).toMatch(pattern);
+        });
+        it("should return 'green', 'red', 'yellow' or 'blue'", function(){
+            expect(generatePattern()).toMatch(pattern);
         });
     });
+
+    // Make sure 'isUnique' function is enforcing the 3 consecutive colour rule
+    describe("Enforce 3 consecutive colour rule", function(){
+        // Apply dummy sequence to 'isUnique' function and checking result
+        it(`'${["green", "yellow", "red", "red", "red"]}' Should return 'false'`, function(){
+            expect(isUnique(["green", "yellow", "red", "red", "red"], "red")).toBe(false);
+        });
+        
+        it(`'${["blue", "yellow", "yellow", "green", "red", "red", "green"]}' Should return 'true'`, function(){
+            expect(isUnique(["blue", "yellow", "yellow", "green", "red", "red", "green"], "green")).toBe(true);
+        }); 
+        
+        it(`'${["blue", "blue", "blue"]}' Should return 'false'`, function(){
+            expect(isUnique(["blue", "blue", "blue"], "blue")).toBe(false);
+        }); 
+
+        it(`'${["red", "yellow"]}' Should return 'true'`, function(){
+            expect(isUnique(["red", "yellow"], "yellow")).toBe(true);
+        }); 
+    });
+
+    // Make sure 'correctInput' function is correctly working
+    describe("Test User input against Simon Sequence", function(){
+        // Dummy variables
+        var simonSequence = [];
+        var userInput = [];
+
+        it(`'Simon sequence: ${["green", "yellow", "red", "red", "red"]} | User Input: ${["green", "yellow", "red", "red"]}' Should return 'true'`, function(){
+            simonSequence = ["green", "yellow", "red", "red", "red"];
+            userInput = ["green", "yellow", "red", "red"];
+            expect(correctInput(simonSequence, userInput)).toBe(true);
+        });
+
+        it(`'Simon sequence: ${["blue", "green", "red", "yellow", "green"]} | User Input: ${["yellow", "yellow", "red", "green"]}' Should return 'false'`, function(){
+            simonSequence = ["blue", "green", "red", "yellow", "green"];
+            userInput = ["yellow", "yellow", "red", "green"];
+            expect(correctInput(simonSequence, userInput)).toBe(false);
+        });
+
+        it(`'Simon sequence: ${["yellow", "red", "green", "blue", "blue"]} | User Input: ${["yellow", "red"]}' Should return 'true'`, function(){
+            simonSequence = ["yellow", "red", "green", "blue", "blue"];
+            userInput = ["yellow", "red"];
+            expect(correctInput(simonSequence, userInput)).toBe(true);
+        });
+
+        it(`'Simon sequence: ${["yellow", "blue", "blue"]} | User Input: ${["green", "blue", "blue"]}' Should return 'false'`, function(){
+            simonSequence = ["yellow", "blue", "blue"];
+            userInput = ["green", "blue", "blue"];
+            expect(correctInput(simonSequence, userInput)).toBe(false);
+        });
+        
+    });
+
 });
--- js/simon.js
+++ js/simon.js
@@ -1,5 +1,15 @@
 
 var isGameActive = false;
+var playerTurn = false;
+var simonSequence = [];
+var playerInput = [];
+
+var greenBeep = new Audio("./assets/sound/simonGreenSound.mp3");
+var redBeep = new Audio("./assets/sound/simonRedSound.mp3");
+var yellowBeep = new Audio("./assets/sound/simonYellowSound.mp3");
+var blueBeep = new Audio("./assets/sound/simonBlueSound.mp3");
+
+var levelCounter = document.getElementById("score-display");
 
 const playButton = document.getElementById("play-btn");
 const restartButton = document.getElementById("restart-btn");
@@ -7,51 +17,156 @@ const greenButton = document.getElementById("simon-green");
 const redButton = document.getElementById("simon-red");
 const yellowButton = document.getElementById("simon-yellow");
 const blueButton = document.getElementById("simon-blue");
-const gameColors = ["", "green", "red", "yellow", "blue"]
-const simonSounds = {green: "./assets/sound/simonGreenSound.mp3",
-                    red: "./assets/sound/simonRedSound.mp3",
-                    yellow: "./assets/sound/simonYellowSound.mp3",
-                    blue: "./assets/sound/simonBlueSound.mp3"};
-const displayCounter = document.getElementById("score-display");
+const gameColors = ["", "green", "red", "yellow", "blue"];
 
-
+// Button event listeners
 playButton.addEventListener("click", startGame);
 restartButton.addEventListener("click", resetGame);
-greenButton.addEventListener("click", playSound);
-redButton.addEventListener("click", playSound);
-yellowButton.addEventListener("click", playSound);
-blueButton.addEventListener("click", playSound);
+greenButton.addEventListener("click", greenPad);
+redButton.addEventListener("click", redPad);
+yellowButton.addEventListener("click", yellowPad);
+blueButton.addEventListener("click", bluePad);
 
+// Start game
 function startGame(){
     isGameActive = true;
     playButton.classList.add("button-hide");
     restartButton.classList.remove("button-hide");
-    displayCounter.innerText = "1";
+    levelCounter.innerText = "1";
+    addSimonSequence();
 }
 
+// Reset game
 function resetGame(){
     document.location.reload();
 }
 
-function playSound(){
-    if(isGameActive == true){
-        switch(this.id){
-            case "simon-green":
-                var test = new Audio(simonSounds["green"]);
-                test.play();
-                break;
-            case "simon-red":
-                var test = new Audio(simonSounds["red"]);
-                test.play();
-                break;
-            case "simon-yellow":
-                var test = new Audio(simonSounds["yellow"]);
-                test.play();
-                break;
-            default:
-                var test = new Audio(simonSounds["blue"]);
-                test.play();
-                break;
+// Play simon sequence
+function playPattern(counter){
+    setTimeout(function(){
+        if(simonSequence[counter] == "green"){
+            greenPad();
+        } else if(simonSequence[counter] == "red"){
+            redPad();
+        } else if(simonSequence[counter] == "yellow"){
+            yellowPad();
+        } else if(simonSequence[counter] == "blue"){
+            bluePad();
+        }
+        counter++;
+        if(counter < parseInt(levelCounter.innerText)){
+            playPattern(counter);
+        }else{
+            console.log("Players turn");
+            playerTurn = true;
+        }
+    }, 1500);
+}
+
+function checkInput(){
+    if(playerInput.length == parseInt(levelCounter.innerText)){
+        if(correctInput(simonSequence, playerInput) == true){
+            levelCounter.innerText = parseInt(levelCounter.innerText) + 1;
+            playerTurn = false;
+            playerInput = [];
+            playPattern(0);
         }
     }
 }
+
+function correctInput(simonSequence, userInput){
+    for(var i=0; i<userInput.length; i++){
+        if(simonSequence[i] != userInput[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Add to simon sequence - max of 20 entries
+function addSimonSequence(){
+    var seqCandidate = "";
+
+    for(var i=0; i<20; i++){
+        seqCandidate = generatePattern();
+
+        while(isUnique(simonSequence, seqCandidate) == false){
+            seqCandidate = generatePattern();
+        }
+        simonSequence.push(seqCandidate);
+    }
+    playPattern(0);
+}
+
+// Randomly generate a colour 
+function generatePattern(){
+    var randomNum;
+    randomNum = Math.floor(Math.random() * 4) + 1;
+
+    return gameColors[randomNum];
+}
+
+// Checks if last 3 entries of simon sequence are the same - enforces 3 same consecutive colour rule
+function isUnique(sequence, testVal){
+    var seqLength = sequence.length;
+
+    if(sequence[seqLength - 1] == testVal && sequence[seqLength - 2] == testVal &&  sequence[seqLength - 3] == testVal){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// Handler for green simon button
+function greenPad() {
+    greenButton.style.backgroundColor = "rgb(150, 255, 150)";
+    setTimeout(function() {
+        greenButton.style.backgroundColor = "green";
+    }, 1000);
+    greenBeep.play();
+    if(isGameActive & playerTurn){
+        playerInput.push("green");
+        checkInput();
+    }
+}
+
+// Handler for red simon button
+function redPad() {
+    redButton.style.backgroundColor = "rgb(255, 150, 150)";
+    setTimeout(function() {
+        redButton.style.backgroundColor = "red";
+    }, 1000);
+    redBeep.play();
+    if(isGameActive & playerTurn){
+        playerInput.push("red");
+        checkInput();
+    }
+}
+
+// Handler for yellow simon button
+function yellowPad() {
+    yellowButton.style.backgroundColor = "rgb(255, 255, 150)";
+    setTimeout(function() {
+        yellowButton.style.backgroundColor = "yellow";
+    }, 1000);
+    yellowBeep.play();
+    if(isGameActive & playerTurn){
+        playerInput.push("yellow");
+        checkInput();
+    }
+}
+
+// Handler for blue simon button
+function bluePad() {
+    blueButton.style.backgroundColor = "rgb(150, 150, 255)";
+    setTimeout(function() {
+        blueButton.style.backgroundColor = "blue";
+    }, 1000);
+    blueBeep.play();
+    if(isGameActive & playerTurn){
+        playerInput.push("blue");
+        checkInput();
+    }
+}
+  
